ae0ecc6733b5c2df39ce37734fa4762c0479c934
KUNAL CHOURE <chourekunal4@gmail.com>
2025-03-14 15:58:44 +0530

    updated

--- Backend/utils/cloudinary.js
+++ Backend/utils/cloudinary.js
@@ -1,15 +1,20 @@
-import { v2 as cloudinary } from "cloudinary";
-import streamifier from "streamifier";
-import dotenv from "dotenv";
+import pkg from 'cloudinary';
+import streamifier from 'streamifier';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Destructure the v2 property from the imported package
+const { v2: cloudinary } = pkg;
+
+// Configure Cloudinary with environment variables
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
     api_key: process.env.CLOUDINARY_API_KEY,  
     api_secret: process.env.CLOUDINARY_API_SECRET  
 });
 
+// Function to upload a buffer to Cloudinary
 const uploadToCloudinary = (buffer) => {
     return new Promise((resolve, reject) => {
         const stream = cloudinary.uploader.upload_stream((error, result) => {
